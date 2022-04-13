a7e9c4ffde83e2e94dbd10a46803b89fb4b6348f
Helena Johansson <helena@klarr.se>
2022-04-13 15:17:38 +0200

    get all users added in test controller

--- src/main/java/com/example/todoapp/controllers/TestController.java
+++ src/main/java/com/example/todoapp/controllers/TestController.java
@@ -1,17 +1,26 @@
 package com.example.todoapp.controllers;
 
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.todoapp.models.User;
+import com.example.todoapp.repository.UserRepository;
+
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/test")
 public class TestController {
+	
+	@Autowired
+	UserRepository userRepository;
 	 
 	 
 	 @GetMapping("/all")
@@ -35,5 +44,15 @@ public class TestController {
 	  @PreAuthorize("hasRole('ADMIN')")
 	  public String adminAccess() {
 	    return "Admin Board.";
+	  }
+	  
+	  @GetMapping("/helloworld")
+	  public String helloWorld() {
+		  return "Hello World! From Spring Boot!";
+	  }
+	  
+	  @GetMapping("/users")
+	  public List <User> getAllUsers() {
+		  return userRepository.findAll();
 	  }
 }
